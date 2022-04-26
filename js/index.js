d6a24cb0c6c80ebaa50b41d26b3bde042ff2ad58
anshdholakia <64213853+anshdholakia@users.noreply.github.com>
2022-04-26 16:35:46 -0400

    Imported JQuery, Created Squares, Updated Navbar

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var boxes = {};
+var graph = {};
+
+class Square{
+    constructor(){
+        this.make_square();
+    }
+
+    make_square() {
+        var square = document.createElement("div");
+        square.classList+="square";
+        // square.style.position = "absolute";
+        // square.style.left = `${this.y}px`;
+        // square.style.top = `${this.x}px`;
+        document.getElementById("graph").appendChild(square);
+        
+    }
+
+}
+
+
+
+// main function
+function main(){
+    var area = $("#graph").height() * $("#graph").width();
+    var no_of_squares = Math.floor(area/Math.pow(34, 2));
+    for(let i = 0; i < no_of_squares; i++){
+        new Square();
+    }
+    // for (let i = document.getElementById("graph").offsetLeft; i < document.getElementById("graph").offsetWidth; i += width) {
+    //     for (let j = document.getElementById("graph").offsetTop; j < document.getElementById("graph").offsetHeight+ document.getElementById("graph").offsetTop; j+=width) {
+    //         console.log(i, j);
+    //         new Square(i, j);
+    //     }
+        
+    // }
+    
+
+}
+main();
+
